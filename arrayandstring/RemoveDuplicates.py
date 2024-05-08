c7c895a56644dd349a533eca96d1b3c28b9b55d4
nhattq <nhattran@vulcanlabs.co>
2024-05-08 10:23:48 +0700

    remove duplicates algorithms

--- arrayandstring/RemoveDuplicates.py
+++ arrayandstring/RemoveDuplicates.py
@@ -1,5 +1,18 @@
 from typing import List
 
 
+# ref: https://leetcode.com/problems/remove-duplicates-from-sorted-array
+# topic: two-pointer, array
 class RemoveDuplicates:
     def removeDuplicates(self, nums: List[int]) -> int:
+        encounter = nums[0]
+        index = 1
+
+        for i in range(1, len(nums)):
+            if encounter != nums[i]:
+                nums[index] = nums[i]
+                encounter = nums[i]
+                index += 1
+
+        return index
+
